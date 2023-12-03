b6b94f378ce5a61f7e0b8981a067f2f08912d164
radzhiv <radzhivkrishna@gmail.com>
2023-12-03 21:18:18 +0530

    feat: minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
+import {X} from "lucide-react";
 import Footer from "./components/Footer";
 import HomePage from "./pages/Home";
 
@@ -9,13 +9,16 @@ function App() {
 
   return (
     <div className="">
-      {/* <div className="text-center text-sm p-3 bg-gray-200 flex justify-center items-center space-x-3">
-        <p className="bg-blue-500 text-white rounded-full mx-1 px-2">New</p>
-        <p className="">Introducing Q&A - a new way to get answers in Notion.</p>
-        <span className="font-bold">
-          Sign up for Q&A waitlist
-        </span>
-      </div> */}
+      <div className="text-center text-sm p-3 bg-gray-200 lg:flex lg:visible hidden">
+        <div className="flex justify-center items-center space-x-3 ml-auto">
+          <p className="bg-blue-500 text-white rounded-full mx-1 px-2">New</p>
+          <p className="">
+            Introducing Q&A - a new way to get answers in Notion.
+          </p>
+          <span className="font-bold">Sign up for Q&A waitlist</span>
+        </div>
+        <X className="ml-auto"/>
+      </div>
       <Navbar />
       <HomePage />
       <Footer />
